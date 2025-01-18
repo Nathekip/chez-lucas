892c4122fd201064b4753705363ca9fcf7d4c2a1
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2025-01-18 13:08:48 +0100

    premier test multi-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist')
 // https://vite.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
-  base: "/chez-lucas"
+  base: "/chez-lucas",
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        bd: resolve(root, 'bd', 'index.html'),
+      }
+    }
+  }
 })
