b6d62ef1efa8b4ef0e8a04767b1466bf49ef1898
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2025-01-18 13:08:04 +0100

    Updates

---
+++ assets/bd-C1HOQoZs.js
@@ -0,0 +1,1 @@
+import{r as t,g as r,j as e,R as o}from"./index-sE4581vx.js";var s=t();const a=r(s);a.render(e.jsx(o.StrictMode,{children:e.jsx("h1",{children:"Welcome to BD !"})}),document.getElementById("root"));
